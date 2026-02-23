922aa6ad23d95d5c50f3fa8a21f13577f012cf59
Marcel Jacek <marcel.jacek@gmail.com>
2026-02-23 17:22:09 +0100

    fix: dockerfiles

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -81,7 +81,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api/live": {
+      "/api": {
         target: "http://localhost:3030",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
